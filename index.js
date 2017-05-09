e1368a3dd9b86b6207aa390d10cca27c4ea0e314
Artem Malko <artem.malko@gmail.com>
2017-05-10 01:06:12 +0700

    Small refactoring, add noop icon for config exporting, add more configuration

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const imagemin = require('imagemin');
 const imageminSvgo = require('imagemin-svgo');
 const loaderUtils = require('loader-utils');
-
 const SvgStorePlugin = require('./lib/SvgStorePlugin');
 
 /**
@@ -12,20 +11,21 @@ const SvgStorePlugin = require('./lib/SvgStorePlugin');
  * @type {Object}
  */
 const DEFAULT_QUERY_VALUES = {
-    name: 'img/sprite.svg',
-    iconName: 'icon-[name]-[hash:5]',
-    spritePath: '',
-    svgoOptions: {
-        plugins: [
-            { collapseGroups: true },
-            { convertPathData: true },
-            { convertStyleToAttrs: true },
-            { convertTransform: true },
-            { removeDesc: true },
-            { removeDimensions: true },
-            { removeTitle: true },
-        ],
-    },
+  name: 'img/sprite.svg',
+  iconName: 'icon-[name]-[hash:5]',
+  spritePath: '',
+  noopIconName: 'noop',
+  svgoOptions: {
+    plugins: [
+      { collapseGroups: true },
+      { convertPathData: true },
+      { convertStyleToAttrs: true },
+      { convertTransform: true },
+      { removeDesc: true },
+      { removeDimensions: true },
+      { removeTitle: true },
+    ],
+  },
 };
 
 /**
@@ -35,54 +35,57 @@ const DEFAULT_QUERY_VALUES = {
  * @param {Buffer} content - the content of the SVG file.
  */
 function loader(content) {
-    const { addDependency, cacheable, resourcePath } = this;
+  const { addDependency, cacheable, resourcePath } = this;
 
-    // Get callback because the SVG is going to be optimized and that is an async operation
-    const callback = this.async();
+  // Get callback because the SVG is going to be optimized and that is an async operation
+  const callback = this.async();
 
-    // Parse the loader query and apply the default values in case no values are provided
-    const query = Object.assign({}, DEFAULT_QUERY_VALUES, loaderUtils.getOptions(this));
+  // Parse the loader query and apply the default values in case no values are provided
+  const query = Object.assign({}, DEFAULT_QUERY_VALUES, loaderUtils.getOptions(this));
 
-    // Add the icon as a dependency
-    addDependency(resourcePath);
+  // Add the icon as a dependency
+  addDependency(resourcePath);
 
-    // Set the loader as not cacheable
-    cacheable(false);
+  // Set the loader as not cacheable
+  cacheable(false);
 
-    // Start optimizing the SVG file
-    imagemin
-        .buffer(content, {
-            plugins: [
-                imageminSvgo(query.svgoOptions),
-            ],
-        })
-        .then((content) => {
+  // Start optimizing the SVG file
+  imagemin
+    .buffer(content, {
+      plugins: [
+        imageminSvgo(query.svgoOptions),
+      ],
+    })
+    .then((content) => {
 
-            // Create the icon name with the hash of the optimized content
-            const iconName = loaderUtils.interpolateName(this, query.iconName, { content });
+      // Create the icon name with the hash of the optimized content
+      const iconName = loaderUtils.interpolateName(this, query.iconName, { content });
 
-            // Register the sprite and icon
-            const icon = SvgStorePlugin.getSprite(query.name).addIcon(resourcePath, iconName, content.toString());
+      // Register the sprite and icon
+      const icon = SvgStorePlugin.getSprite(query.name).addIcon(resourcePath, iconName, content.toString());
 
-            // Export the icon as a metadata object that contains urls to be used on an <img/> in HTML or url() in CSS
-            callback(
-                null,
-                `
-                var iconDimensions = ${icon.getDocument().getDimensions()};
-                module.exports = {
-                    symbol: '${query.spritePath}${icon.getUrlToSymbol()}',
-                    view: '${query.spritePath}${icon.getUrlToView()}',
-                    width: '${iconDimensions.width}',
-                    height: '${iconDimensions.height}',
-                    toString: function () {
-                        return JSON.stringify(this.view);
-                    }
-                };`
-            );
-        })
-        .catch((err) => {
-            callback(err);
-        });
+      let iconMetaData = '';
+
+      if (iconName.indexOf(`-${query.noopIconName}-`) > -1) {
+        iconMetaData = `
+          module.exports = {
+            spriteFileName: '${icon.getSpriteFileName()}'
+          };`;
+      } else {
+        iconMetaData = `
+          module.exports = {
+              id: '${icon.getSymbolID()}',
+              width: '${icon.getDocument().getDimensions().width}',
+              height: '${icon.getDocument().getDimensions().height}'
+          };`;
+      }
+
+      // Export the icon as a metadata object that contains urls to be used on an <img/> in HTML or url() in CSS
+      callback(null, iconMetaData);
+    })
+    .catch((err) => {
+      callback(err);
+    });
 }
 
 loader.raw = true;
